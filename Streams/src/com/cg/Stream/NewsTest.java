0f52322f9cc43baebf22008b3ab05100be4b32a0
sahithinadimpalli <47311558+sahithinadimpalli@users.noreply.github.com>
2019-02-20 18:32:04 +0530

    Add files via upload

--- Streams/src/com/cg/Stream/NewsTest.java
+++ Streams/src/com/cg/Stream/NewsTest.java
@@ -5,31 +5,51 @@ package com.cg.Stream;
 	import java.util.Collections;
 	import java.util.Comparator;
 	import java.util.List;
-	import java.util.function.Predicate;
+import java.util.Map;
+import java.util.function.Predicate;
 	import java.util.stream.Collectors;
 
 	public class NewsTest {
 
 		public static void main(String[] args) {
 			
-				List<News> list=new ArrayList<>();
-				list.add(new News(101,"xyz","The budget is an vote on acccount","Yeah True"));
-				list.add(new News(102,"abc","Election notification yet to be released ","It will release by March"));
-				list.add(new News(103,"def","sports","IPL "));
-				list.add(new News(103,"xyz","budget","The budget is 23LAKH CRORES"));
-				list.add(new News(101,"xyz","sports","First IPL mmatch is between CSK and RCB"));
-				list.add(new News(101,"xyz","budget","The budget is an vote on acccount"));
-				//->nPredicate<String> p= n->n.contentEquals("budget");
-			List<News> list1=list.stream().filter(n->n.getComment().contains("budget")).collect(Collectors.toList());
-			long l=list1.stream().count();
-			System.out.println("The word budget in comments"+l);
-			//System.out.println("-------------------------------------------");
-			//List<News> list2=list.stream().filter(n->n.getNewsId().).collect(Collectors.toList());
 			
+				List<News>list = new ArrayList<>();
+				list.add(new News(101,"Xyz","This is a vote on account","Yes true"));
+				list.add(new News(103,"Def","Sports","budget"));
+				list.add(new News(101,"Abp","Provides news worldwide","budget"));
+				list.add(new News(102,"Abc","Get exclusive videos","Entertainment"));
+				list.add(new News(101,"Xyz","Sports","budget is an account"));
 				
+				System.out.println("Assignment 5----------------------------------------------");
 				
-
+				List<News>list1=list.stream()
+						.filter(n->n.getComment().contains("budget"))
+						.collect(Collectors.toList());
+				long l=list1.parallelStream().count();
+				System.out.println("The word budget in comments: "+l);
+				
+				System.out.println("Assignment 4----------------------------------------------");
+				
+				Map<Object, Long> map = list.stream().collect(Collectors.groupingBy(News::getNewsId, Collectors.counting()));
+			    long maxval=(Collections.max(map.values()));
+			    for(java.util.Map.Entry<Object, Long> entry:map.entrySet()) {
+			    	if(entry.getValue()==maxval) {
+			    		System.out.println(entry.getKey());
+			    	}
+			    }
+			    System.out.println("Assignment 6----------------------------------------------");
+			    Map<Object, Long> map2 = list.stream().collect(Collectors.groupingBy(News::getPostedByUser, Collectors.counting()));
+			    long maxval2=(Collections.max(map2.values()));
+			    for(java.util.Map.Entry<Object, Long> entry:map2.entrySet()) {
+			    	if(entry.getValue()==maxval2) {
+			    		System.out.println(entry.getKey());
+			    	}
+			    }
+			    System.out.println("Assignment 7----------------------------------------------");
+			    Map<Object, Long> map3 = list.stream().collect(Collectors.groupingBy(News::getCommentByUser, Collectors.counting()));
+			    System.out.println(map3);
+			    
 		}
-
-	}
+		}
 
